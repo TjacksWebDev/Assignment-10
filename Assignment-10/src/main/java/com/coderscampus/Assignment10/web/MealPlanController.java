c745954fe8850edd0e01ace2cf6b9582b201d9d3
Ty Jackson <143833425+TjacksWebDev@users.noreply.github.com>
2024-01-10 18:36:47 -0500

    Update MealPlanController.java
    IMPORTED 
    import org.springframework.beans.factory.annotation.Value;

--- Assignment-10/src/main/java/com/coderscampus/Assignment10/web/MealPlanController.java
+++ Assignment-10/src/main/java/com/coderscampus/Assignment10/web/MealPlanController.java
@@ -2,6 +2,7 @@ package com.coderscampus.Assignment10.web;
 
 import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
